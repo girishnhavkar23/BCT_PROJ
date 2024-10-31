013366ca8c4868be1b2dd988947acd383c154c52
SHREYA PANIKKASSERY <114416188+Shreyap1413@users.noreply.github.com>
2024-10-31 18:22:46 +0000

    added in _app

--- BCT_WEB/pages/_app.js
+++ BCT_WEB/pages/_app.js
@@ -1,9 +1,16 @@
+import Head from "next/head";
+import toast, {Toaster} from "react-hot-toast";
 import "../styles/globals.css";
 
+//INTERNAL IMPORT
+import {StateContextProvider} from "../.next/index"
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
+      <StateContextProvider>
+        <Component {...pageProps} />
+        <Toaster />
+      </StateContextProvider>
     </>
   );
 }
