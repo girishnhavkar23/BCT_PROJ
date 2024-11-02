625d917b73a50318dab64342ef081ee4a25ff941
Girish Nhavkar <124400416+girishnhavkar23@users.noreply.github.com>
2024-11-02 09:16:25 +0530

    Update Footer.jsx

--- BCT_WEB/Components/Footer.jsx
+++ BCT_WEB/Components/Footer.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer-menu">
+          <div className="copyright">&copy; 2024 icoMarketplace all rights reserved</div>
+        </div>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Creator</a>
+          </li>
+          <li>
+            <a href="/">ICO</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
